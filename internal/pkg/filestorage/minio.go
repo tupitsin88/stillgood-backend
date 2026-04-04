cb4408eb5461be034673185335ed13dcdcde4737
Timofey Tupitsin <trtupitsin@edu.hse.ru>
2026-04-04 14:51:41 +0300

    Add supporting download from compressed images and creating public urls

--- internal/pkg/filestorage/minio.go
+++ internal/pkg/filestorage/minio.go
@@ -1,10 +1,13 @@
 package filestorage
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"mime/multipart"
 	"path/filepath"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/minio/minio-go/v7"
@@ -12,13 +15,14 @@ import (
 )
 
 type FileStorage struct {
-	client     *minio.Client
-	bucketName string
-	endpoint   string
-	useSSL     bool
+	client        *minio.Client
+	bucketName    string
+	endpoint      string
+	useSSL        bool
+	publicBaseURL string
 }
 
-func NewFileStorage(endpoint, accessKey, secretKey, bucketName string, useSSL bool) (*FileStorage, error) {
+func NewFileStorage(endpoint, accessKey, secretKey, bucketName string, useSSL bool, publicBaseURL string) (*FileStorage, error) {
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
 		Secure: useSSL,
@@ -28,10 +32,11 @@ func NewFileStorage(endpoint, accessKey, secretKey, bucketName string, useSSL bo
 	}
 
 	return &FileStorage{
-		client:     minioClient,
-		bucketName: bucketName,
-		endpoint:   endpoint,
-		useSSL:     useSSL,
+		client:        minioClient,
+		bucketName:    bucketName,
+		endpoint:      endpoint,
+		useSSL:        useSSL,
+		publicBaseURL: strings.TrimRight(publicBaseURL, "/"),
 	}, nil
 }
 
@@ -42,15 +47,33 @@ func (fs *FileStorage) Upload(fileHeader *multipart.FileHeader) (string, error) 
 	}
 	defer file.Close()
 
-	ext := filepath.Ext(fileHeader.Filename)
-	filename := fmt.Sprintf("%s%s", uuid.New().String(), ext)
+	ext := strings.ToLower(filepath.Ext(fileHeader.Filename))
 	contentType := fileHeader.Header.Get("Content-Type")
 
-	_, err = fs.client.PutObject(context.Background(), fs.bucketName, filename, file, fileHeader.Size, minio.PutObjectOptions{
+	return fs.uploadReader(file, fileHeader.Size, ext, contentType)
+}
+
+func (fs *FileStorage) UploadBytes(content []byte, ext string, contentType string) (string, error) {
+	reader := bytes.NewReader(content)
+	return fs.uploadReader(reader, int64(len(content)), ext, contentType)
+}
+
+func (fs *FileStorage) uploadReader(reader io.Reader, size int64, ext string, contentType string) (string, error) {
+	filename := fmt.Sprintf("%s%s", uuid.New().String(), ext)
+
+	_, err := fs.client.PutObject(context.Background(), fs.bucketName, filename, reader, size, minio.PutObjectOptions{
 		ContentType: contentType,
 	})
 	if err != nil {
 		return "", err
+	}
+
+	return fs.publicURL(filename), nil
+}
+
+func (fs *FileStorage) publicURL(filename string) string {
+	if fs.publicBaseURL != "" {
+		return fmt.Sprintf("%s/%s/%s", fs.publicBaseURL, fs.bucketName, filename)
 	}
 
 	protocol := "http"
@@ -58,5 +81,5 @@ func (fs *FileStorage) Upload(fileHeader *multipart.FileHeader) (string, error) 
 		protocol = "https"
 	}
 
-	return fmt.Sprintf("%s://%s/%s/%s", protocol, fs.endpoint, fs.bucketName, filename), nil
+	return fmt.Sprintf("%s://%s/%s/%s", protocol, fs.endpoint, fs.bucketName, filename)
 }
